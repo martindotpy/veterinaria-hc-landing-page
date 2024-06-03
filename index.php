378b54401378a8ffb291bc4a067d74b542d2d9c6
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-03 18:00:48 -0500

    FEAT: Uncompleted page 1

--- index.php
+++ index.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <title>Document</title>
-
+    <title>Veterinaria HC - SPA & Pet Shop</title>
 </head>
 <body>
-    <?= "Hola mundo" ?>
+    <header>
+        <div class="_logo">
+            <img src="" alt="">
+        </div>
+        <div>
+            <button class="headerButton">Nosotros</button>
+            <button class="headerButton">Servicios</button>
+            <button class="headerButton">Ubicación</button>
+        </div>
+        <div>
+            <button>Contáctanos</button>
+        </div>
+    </header>
     <script src="/assets/js/app.js"></script>
 </body>
 </html>
