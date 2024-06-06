09cbbe175a4971a64bd8537acb7820a327bb93af
ZoombieAlc <zoomiealc@gmail.com>
2024-06-05 23:43:19 -0500

    home page basic style

--- index.php
+++ index.php
@@ -17,9 +17,87 @@
             <a class="headerButton" href="#">Ubicación</a>
         </div>
         <div>
-            <button>Contáctanos</button>
+            <a class="button">Contáctanos</a>
         </div>
     </header>
+    <div class="container">
+        <div class="about-container">
+            <div class="text">
+                <h1>¿Quiénes somos?</h1>
+                <p>Somos una clínica que se preocupa por las mascotas tanto
+                    <br>como tú. Con una dedicación apasionada por el bienestar animal,
+                    <br>nuestro equipo está capacitado para atender las principales necesidades
+                    <br>de tu mascota.
+                    <br>¡Bienvenidos a la familia de nuestra clínica veterinaria local!
+                </p>
+                <a href="about-us.php" class="button">Contáctanos</a>
+            </div>
+            <img src="assets\img\dogflower.png" alt="">
+        </div>
+        <hr>
+        <div class="services">
+            <h1>Nuestros servicios</h1>
+            <p>Ofrecemos múltiples servicios, la salud de tu mascota nos importa
+                <br>y queremos darle los mejores cuidados</p>
+            <div class="cards">
+                <div class="card">
+                    <h3>Spa <br> Veterinario</h3>
+                    <p>Ofrecemos tratamientos de bienestar diseñados
+                    <br> para mimar a su mascota 
+                    <br>y mantenerla feliz y saludable. </p>
+                </div>
+                <div class="card">
+                    <h3>Consultas <br>integrales</h3>
+                    <p>Realizamos consultas exhautivas para 
+                    <br> evaluar la salud y bienestar de su mascota.
+                    <br>Desde exámenes de rutina hasta diagnósticos,
+                <br>la salud de su mascota es nuestra prioridad. </p>
+                </div>
+                <div class="card">
+                    <h3>Precios bajos</h3>
+                    <p>Ofrecemos productos para su mascota, 
+                    <br>desde juguetes hasta  productos para el
+                    <br>cuidado de tu mascota.   </p>
+                </div>
+            </div>
+            <div class="img">
+                <img src="assets\img\cathome.png" alt="">
+            </div>
+            
+        </div>
+        <hr>
+        <div class="location">
+            <div class="info">
+                <div>
+                <h2>Local y Contacto</h2>
+                <p>Calle Las Palmeras-Ferreñafe</p>
+                <p>+51 922 362 525</p>
+                </div>
+                <div>
+                <h2>Horarios</h2>
+                <p>Lunes a viernes</p>
+                <p>8:00 am - 9:00 pm</p>
+                <p>Sábado y domingo</p>
+                <p>9:00 am - 6:00 pm</p>
+                </div>
+            </div>
+            <img src="assets\img\doghome.png" alt="">
+            <div class="map"></div>
+        </div>
+
+    </div>
+
+    <footer>
+         <div class="logo">
+            <img src="assets\img\logo_dark.png" alt="">
+        </div>
+        <div class="headerButtons">
+            <a class="button" href="#">Nosotros</a>
+            <a class="button" href="#">Servicios</a>
+            <a class="button" href="#">Ubicación</a>
+        </div>
+        <div>
+    </footer>
     <script src="/assets/js/app.js"></script>
 </body>
 </html>
